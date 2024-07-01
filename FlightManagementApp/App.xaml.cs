53520cbb5f7fdd29766d92ba0ac64a9fabae8131
faruqonaayo <onaayofaruq24@gmail.com>
2024-06-30 23:24:23 -0600

    Add project files.

---
+++ FlightManagementApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightManagementApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FlightManagementApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlightManagementApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
